5a075d8c9c2b442883decfd811861a71cdf8cda2
Kyle Stadler <kstad4@gmail.com>
2022-11-02 21:52:25 -0500

    Developed passing tests in step 4

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,49 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId() {
+        Job testJob = new Job();
+        Job testJob2 = new Job();
+        assertNotEquals(testJob.getId(), testJob2.getId());
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob3 instanceof Job);
+        assertTrue(testJob3.getEmployer() instanceof Employer);
+        assertTrue(testJob3.getLocation() instanceof Location);
+        assertTrue(testJob3.getCoreCompetency() instanceof CoreCompetency);
+        assertTrue(testJob3.getPositionType() instanceof PositionType);
+
+        assertEquals("Product tester", testJob3.getName());
+        assertEquals("ACME", testJob3.getEmployer().toString());
+        assertEquals("Desert", testJob3.getLocation().toString());
+        assertEquals("Quality control", testJob3.getPositionType().toString());
+        assertEquals("Persistence", testJob3.getCoreCompetency().toString());
+
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job testJob4 = new Job("Canine specialist", new Employer("PetCo"), new Location("Kansas City"), new PositionType("Animal science"), new CoreCompetency("Patience"));
+        Job testJob5 = new Job("Canine specialist", new Employer("PetCo"), new Location("Kansas City"), new PositionType("Animal science"), new CoreCompetency("Patience"));
+
+        assertFalse(testJob4.equals(testJob5));
+    }
 
 }
